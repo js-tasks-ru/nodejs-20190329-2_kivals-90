9e77552bbbfd6c16bd14bff0b347c127ac045ccd
melnik <melnik@bifit.com>
2019-04-17 12:34:21 +0300

    add solution.txt for 4-module

--- 4-module/3-task/server.js
+++ 4-module/3-task/server.js
@@ -1,17 +1,37 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
+const messages = require('../server_messages');
 
 const server = new http.Server();
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
-
   const filepath = path.join(__dirname, 'files', pathname);
+
+  const doResponse = (code) => {
+    res.statusCode = code;
+    res.end(messages[code]);
+  };
+
+  if (pathname.includes('\/')) {
+    doResponse(400);
+  }
+
+  const deleteFile = (path) => {
+    fs.unlink(path, handleDelete);
+  };
+
+  const handleDelete = (err) => {
+    err ?
+      doResponse(err.code === 'ENOENT'? 404 : 500) :
+      doResponse(200);
+  };
 
   switch (req.method) {
     case 'DELETE':
-
+      deleteFile(filepath);
       break;
 
     default:
---
+++ 4-module/server_messages.js
@@ -0,0 +1,10 @@
+module.exports = {
+  200: 'File has deleted successfully',
+  201: 'File has uploaded successfully',
+  400: 'Incorrect file path',
+  404: 'File not found',
+  409: 'File has already existed',
+  413: 'File is too big',
+  500: 'Internal server error',
+};
+
